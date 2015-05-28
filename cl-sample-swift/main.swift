320841e5c6dc249f293e5d098745f2e215577c41
Yusuke Ohashi <yusuke@junkpiano.me>
2015-05-29 02:57:49 +0900

    Initial Commit

---
+++ cl-sample-swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  cl-sample-swift
+//
+//  Created by Ohashi Yusuke on 5/29/15.
+//  Copyright (c) 2015 Ohashi Yusuke. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
